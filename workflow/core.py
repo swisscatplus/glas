5bcdcc3b2777f83b1ce1a785e3cc5d034e0375c7
alec.schmidt <alec.schmidt@etu.hesge.ch>
2024-05-17 13:31:51 +0200

    removed: workflow core verbose and static

--- workflow/core.py
+++ workflow/core.py
@@ -5,7 +5,7 @@ from .models import WorkflowModel
 
 
 class Workflow:
-    def __init__(self, _id: int, name: str, steps: list[BaseNode], verbose: bool = False) -> None:
+    def __init__(self, _id: int, name: str, steps: list[BaseNode]) -> None:
         if len(steps) < 2:
             raise ValueError("Steps must at least have 2 nodes")
         self.id = _id
@@ -13,10 +13,6 @@ class Workflow:
         self.steps = steps
         self.source = steps[0]
         self.destination = steps[-1]
-
-        self.verbose = verbose
-
-        self.stop_flag = False
 
     def __repr__(self) -> str:
         return f"{self.source} - {self.destination}"
