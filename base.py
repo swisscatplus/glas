86653dc0c55ae7de964e0bd5407ef5395aaf6819
ToguyC <tanguy.cvgn@gmail.com>
2024-06-07 13:44:58 +0200

    update: CORS policy

--- base.py
+++ base.py
@@ -22,7 +22,8 @@ class BaseScheduler:
         self.api = FastAPI(lifespan=lifespan)
         self.api.add_middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:5173"],
+            allow_origins=["*"],
+            allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
         )
