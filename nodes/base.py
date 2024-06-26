d75cae3b0208ce182774544ad56c1bf265a0bdb3
alec.schmidt <alec.schmidt@etu.hesge.ch>
2024-06-26 14:22:18 +0200

    feat: node method set_in_use()

--- nodes/base.py
+++ nodes/base.py
@@ -223,3 +223,6 @@ class BaseNode(ABCBaseNode):
 
     def set_available(self) -> None:
         self.state = NodeState.AVAILABLE
+
+    def set_in_use(self) -> None:
+        self.state = NodeState.IN_USE
